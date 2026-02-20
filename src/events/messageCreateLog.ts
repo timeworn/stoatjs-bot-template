a57cfa8e1d8c22a6708da2c16af36811db93626b
timeworn <contact@timeworn.net>
2026-02-20 12:30:10 -0800

    Delete src/events/messageCreateLog.ts

--- src/events/messageCreateLog.ts
+++
@@ -1,9 +0,0 @@
-import { Event } from "@/classes/event";
-
-export default new Event({
-  name: "messageCreate",
-  description: "Logs every message sent in the server",
-  execute: (client, message) => {
-    console.log(`${message.author?.username}: ${message.content}`);
-  },
-});
